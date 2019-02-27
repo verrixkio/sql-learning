0b43aa1bb40378fe68e435f6791c5e2d2cc5d836
verrixkio <verrixkio@gmail.com>
2019-02-27 00:40:50 +0000

    added migration for updating files

---
+++ knexfile.js
@@ -0,0 +1,48 @@
+// Update with your config settings.
+const settings = require('./settings');
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: { 
+      user: settings.user, 
+      database: settings.database, 
+      password: settings.password
+    },
+  
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
